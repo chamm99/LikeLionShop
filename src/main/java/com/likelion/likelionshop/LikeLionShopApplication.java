2138079b00c5c220ae5499d622a5cd4765459c45
chamm99 <alslalsl3125@gmail.com>
2024-03-29 02:13:12 +0900

    1주차 과제

---
+++ src/main/java/com/likelion/likelionshop/LikeLionShopApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.likelionshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeLionShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikeLionShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/likelionshop/LikeLionShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.likelionshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeLionShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
