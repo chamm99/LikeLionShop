4635c0392754b08058fcf7ad659191c54dc1efd9
chamm99 <alslalsl3125@gmail.com>
2024-04-03 17:04:59 +0900

    update: controller url mapping 추가

--- src/main/java/com/likelion/likelionshop/Controller/OrderController.java
+++ src/main/java/com/likelion/likelionshop/Controller/OrderController.java
@@ -12,28 +12,28 @@ import java.util.List;
 public class OrderController {
 
     // 1. 주문을 생성하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 생성하기"입니다. -> 주문은 리스트 형태로 요청을 보내주세요!
-    @PostMapping
+    @PostMapping("/post")
     public String createOrder(
             @RequestBody List<CreateOrderRequestDto> requestDtoList) {
         return "주문 생성하기";
     }
 
     // 2. 주문을 가져오는 컨트롤러를 만듭니다. 이때 return 값은 "주문 가져오기"입니다.
-    @GetMapping
+    @GetMapping("")
     public String getOrder(
             @RequestBody CreateOrderRequestDto requestDto) {
         return "주문 가져오기";
     }
 
     // 3. 주문을 수정하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 수정하기"입니다.
-    @PutMapping
+    @PutMapping("/update")
     public String updateOrder(
             @RequestBody UpdateOrderRequestDto requestDto) {
         return "주문 수정하기";
     }
 
     // 4. 주문을 삭제하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 삭제하기"입니다.
-    @DeleteMapping
+    @DeleteMapping("/delete")
     public String deleteOrder(
             @RequestBody UpdateOrderRequestDto requestDto) {
         return "주문 삭제하기";
--- src/main/java/com/likelion/likelionshop/Controller/UserController.java
+++ src/main/java/com/likelion/likelionshop/Controller/UserController.java
@@ -12,7 +12,7 @@ public class UserController {
 
     // 1. 사용자를 생성하는 컨트롤러를 만듭니다.
     // 이때 log.info를 이용하여 사용자의 이름, 주소, ID, PW를 출력해줍니다. return 값은 "사용자 생성"입니다.
-    @PostMapping
+    @PostMapping("/post")
     public String createUser(
             @RequestBody CreateUserRequestDto requestDto) {
         log.info(requestDto.getName());
@@ -24,7 +24,7 @@ public class UserController {
 
     // 2. 사용자를 조회하는 컨트롤러를 만듭니다.
     // 이때 log.info를 이용하여 사용자의 ID를 출력해줍니다. return 값은 "사용자 조회"입니다.
-    @GetMapping
+    @GetMapping("")
     public String getUser(
             @RequestBody CreateUserRequestDto requestDto) {
         log.info(requestDto.getId());
@@ -33,7 +33,7 @@ public class UserController {
 
     // 3. 사용자를 수정하는 컨트롤러를 만듭니다.
     // 이때 log.info를 이용를여 사용자의 이름, 주소를 출력해줍니다. return 값은 "사용자 수정"입니다.
-    @PutMapping
+    @PutMapping("/update")
     public String updateUser(
             @RequestBody UpdateUserRequestDto requestDto) {
         log.info(requestDto.getName());
@@ -43,7 +43,7 @@ public class UserController {
 
     // 4. 사용자를 삭제하는 컨트롤러를 만듭니다.
     // 이때 log.info 이용하여 사용자의 ID를 출력해줍니다. return 값은 "사용자 삭제"입니다.
-    @DeleteMapping
+    @DeleteMapping("/delete")
     public String deleteUser(
             @RequestBody CreateUserRequestDto requestDto) {
         log.info(requestDto.getId());
